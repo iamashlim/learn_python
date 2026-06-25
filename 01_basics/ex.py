03dd893fe08c699e814f89bdf52cc8b5e6edde6d
Ashlim Tamang <ashlimtamang7@gmail.com>
2026-06-25 07:46:37 +0000

    Solve exercises from chapter 2 of P4ev

---
+++ 01_basics/ex.py
@@ -0,0 +1,22 @@
+#ex_2 program that uses input to prompt a user for thier name and then welcomes them
+
+inp = input('Enter your name:\n')
+print("Welcom ",inp)
+
+
+#ex_3 program for calculation gross pay
+
+hours = float(input('Enter hours : '))
+rate = float(input('Enter rate : '))
+pay = hours * rate
+print(pay)
+
+#ex_4 
+
+width = 17
+height  = 12.0
+
+print(width//2)
+print(width/2.0)
+print(height/3)
+print(1+2*5)
