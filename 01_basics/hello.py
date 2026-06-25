48f0901fd17a819aa9e746b290d8995f1957c64c
Ashlim Tamang <ashlimtamang7@gmail.com>
2026-06-25 07:44:25 +0000

    Learn additonal concepts

--- 01_basics/hello.py
+++ 01_basics/hello.py
@@ -1,1 +1,24 @@
 print("Hello, Python!")
+x = 4
+print(type (x))
+print(type('I am good'))
+
+print(1,00,00)
+
+## Division 
+
+print(10/3)
+print(10//3)
+
+
+##Test
+
+5 
+a = 5
+a + 1
+
+
+## Asking the user for input
+
+inp = input('Enter something you like:')
+print(inp)
